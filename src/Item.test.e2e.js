1b870997847fcb1bc9d5a7cff887744dd3ab81b8
Alexandre Piel <alexandre.piel@gmail.com>
2019-01-27 21:13:17 +0100

    first attempt to loop data

--- src/Item.test.e2e.js
+++ src/Item.test.e2e.js
@@ -1,16 +1,32 @@
+// import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
-const names = ['Mina Watsica', 'Cassandra O\'Hara'];
+let data = [];
 
 describe('Item', () => {
-    names.forEach((name, index) => {
-        describe(`Item ${name} at index ${index}`, () => {
-            beforeAll(async () => {
-                await page.goto(`http://localhost:3000/item/${index}`);
-            });
+    beforeAll(async () => {
+        const response = await fetch('http://localhost:2000/');
+        data = await response.json();
+    });
 
-            it(`should display ${name} text on page`, async () => {
-                await expect(page).toMatch(name);
-            });
+    // it('hello', () => {});
+
+    // data.forEach((item, index) => {
+    for(let index = 0; index < 100; index++) {
+        testIndex(index);
+    }
+    // });
+});
+
+function testIndex(index) {
+    describe(`Item at index ${index}`, () => {
+        beforeAll(async () => {
+            await page.goto(`http://localhost:3000/item/${index}`);
+        });
+
+        it('should display name text on page', async () => {
+            const item = data[index];
+            await expect(page).toMatch(item.name);
         });
     });
-});
+}
